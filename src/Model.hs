cc95b86f80a317ccf0c9f4df3296988294d87c6a
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-10 14:40:14 +0200

    Added Data Types
    Co-authored-by: woekkk <woekkk@users.noreply.github.com>

--- src/Model.hs
+++ src/Model.hs
@@ -16,3 +16,45 @@ data GameState = GameState {
 
 initialState :: GameState
 initialState = GameState ShowNothing 0
+
+
+--Now the data types we made ourselves:
+
+data Player = Player Point BoundingBox Lives
+
+data Enemy = Enemy Point BoundingBox Lives
+type AliveEnemies = [Enemy]
+
+data Bullet = Bullet Point BoundingBox Point Owner
+type ShotBullets = [Bullet]
+data Owner = Friendly | Hostile
+
+type Lives = Int
+type Score = Int
+
+data IsPaused = NotPaused | Paused
+
+data PlayingStatus = MainMenu | LevelMenu | PlayingLevel Level
+
+--TECHNICALLY OPTIONAL
+data Wall          = Wall Point BoundingBox
+type MovingEnemy   = Enemy
+type Walls         = [Wall]
+type MovingEnemies = [MovingEnemy]
+
+data Level = Level LevelNr Enemies
+type LevelNr = Int
+type Enemies = [(Enemy, EnterTime)]
+type EnterTime = Int
+--END TECHNICALLY OPTIONAL
+
+data Point  = Point Float Float
+data Vector = Vector Float Float
+
+type Playtime = Float
+
+data BoundingBox = BoundingBox { 
+                    lowerLeft :: Point
+                  , width     :: Int
+                  , height    :: Int 
+                  }
